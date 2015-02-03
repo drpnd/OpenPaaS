2617e2c676e628d4cfc9fa94f5e4f0fa2b155f16
Hirochika Asai <panda@jar.jp>
2015-02-03 22:51:33 +0900

    add uniqueness validation.

--- repos/oplat/app/models/user.rb
+++ repos/oplat/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :repositories
-  validates :name, presence: true, length: { minimum: 5, maximum: 40 }
+  validates :name, presence: true, length: { minimum: 5, maximum: 40 },
+  uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
   uniqueness: { case_sensitive: false }
