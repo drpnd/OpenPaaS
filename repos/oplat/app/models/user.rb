7ba7d0705f0d55ca2e71bd1f3f8a4436373f6e67
Hirochika Asai <panda@jar.jp>
2015-02-03 22:46:46 +0900

    add simple email validation.

--- repos/oplat/app/models/user.rb
+++ repos/oplat/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :repositories
-  validates :name, length: { minimum: 5, maximum: 40 }
+  validates :name, presence: true, length: { minimum: 5, maximum: 40 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
