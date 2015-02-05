4196b387676f933547b02c5d25f879d0d18f6a6c
Hirochika Asai <panda@jar.jp>
2015-02-06 02:50:30 +0900

    modify the root page.

--- repos/oplat/config/routes.rb
+++ repos/oplat/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users,             only: [:new, :create]
   resources :sessions,          only: [:new, :create, :destroy]
   resources :repositories,      only: [:new, :create]
 
