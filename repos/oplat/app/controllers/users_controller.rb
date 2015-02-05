8edc9c5e9fbb062710b4dc92f4af08187fab7e3f
Hirochika Asai <panda@jar.jp>
2015-02-06 02:44:54 +0900

    sign in and redirect after sign-up.

--- repos/oplat/app/controllers/users_controller.rb
+++ repos/oplat/app/controllers/users_controller.rb
@@ -26,14 +26,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to Open PaaS!"
+      redirect_to root_url
+    else
+      render 'new'
     end
   end
 
