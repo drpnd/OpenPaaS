9af65fc56520a60146b69b565a58f1c4197085d5
Hirochika Asai <panda@jar.jp>
2015-02-06 16:48:39 +0900

    repositories.

--- repos/oplat/app/models/repository.rb
+++ repos/oplat/app/models/repository.rb
@@ -23,7 +23,8 @@ class Repository < ActiveRecord::Base
 
   private
   def create_repository
-    cmd = "sudo -u '#{ENV['OPLAT_OPLAT_GITOLITE_USER'].shellescape}' #{Rails.root}/scripts/create_repository.rb #{current_user.name.shellescape} #{name.shellescape} #{db_password.shellescape} #{ENV['OPLAT_OPLAT_EXT_DATABASE_NET'].shellescape}"
+    uesr = User.find(:user_id)
+    cmd = "sudo -u '#{ENV['OPLAT_OPLAT_GITOLITE_USER'].shellescape}' #{Rails.root}/scripts/create_repository.rb #{user.name.shellescape} #{name.shellescape} #{db_password.shellescape} #{ENV['OPLAT_OPLAT_EXT_DATABASE_NET'].shellescape}"
     logger.info cmd
     system( cmd )
   end
