2b2204ede52885937f36422d0433be7393c172ec
Hirochika Asai <panda@jar.jp>
2015-02-09 17:12:21 +0900

    fix.

--- lxc/clone.sh
+++ lxc/clone.sh
@@ -4,7 +4,7 @@ VERSION=00
 
 for i in `seq 200`;
 do
-    n=`printf %03 $i`
-    lxc-clone -o template -n instance-v00-$n
-    echo "lxc.network.ipv4 = 10.0.1.$n/8" >> /var/lib/lxc/instance-v00-$n/config
+    n=`printf "%03d" $i`
+    lxc-clone -o template -n instance-v$VERSION-$n
+    echo "lxc.network.ipv4 = 10.0.1.$n/8" >> /var/lib/lxc/instance-v$VERSION-$n/config
 done
