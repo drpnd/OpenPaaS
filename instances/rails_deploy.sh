9d5f72790910f48787ed65038ee58e38849b2d57
Hirochika Asai <panda@jar.jp>
2015-02-09 16:08:32 +0900

    add scripts for instances.

---
+++ instances/rails_deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+export RAILS_ENV=$1
+export MYAPP_DATABASE_PASSWORD=$2
+
+## The following variables may be moved to arguments of this script.
+REPOS_SERVER=$3
+REPOS_USERNAME=$4
+REPOS_APPNAME=$5
+
+## Kill the running instance
+kill `cat rails_instance.pid`
+
+## Remove the old software
+rm -rf $REPOS_APPNAME
+
+## Clone the up-to-date software
+git clone ssh://git@$REPOS_SERVER/$REPOS_USERNAME/$REPOS_APPNAME
+
+## Execute bundle install and DB migration
+cd $REPOS_APPNAME
+bundle install --path vendor/bundle
+bundle exec rake db:migrate
---
+++ instances/rails_instance.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+export RAILS_ENV=$1
+export MYAPP_DATABASE_PASSWORD=$2
+export SECRET_TOKEN=$3
+
+## The following variable may be moved to arguments of this script.
+REPOS_APPNAME=$4
+
+## Kill the running instance
+kill `cat rails_instance.pid`
+
+cd $REPOS_APPNAME
+
+## Run the rails server
+bundle exec rails server &
+
+## Save the PID if rails server and watch this shell process until the rails server goes down
+PID=$!
+cd ..
+echo $PID > rails_instance.pid
+
+RET=0
+while [ $RET -eq 0 ];
+do
+	kill -0 $PID
+	RET=$?
+	sleep 1
+done
