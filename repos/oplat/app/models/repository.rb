9f27ac2d43bf18d3d686b2066b1eb195ac029140
Hirochika Asai <panda@jar.jp>
2015-02-17 22:39:58 +0900

    update.

--- repos/oplat/app/models/repository.rb
+++ repos/oplat/app/models/repository.rb
@@ -11,7 +11,8 @@ class Repository < ActiveRecord::Base
 
   before_create {|repository|
     repository.secret_token = SecureRandom.hex(64)
-    repository.db_password = SecureRandom.base64(15)}
+    #repository.db_password = SecureRandom.base64(15)}
+    repository.db_password = SecureRandom.hex(12)}
 
   before_create :create_repository
   after_create :create_instance
